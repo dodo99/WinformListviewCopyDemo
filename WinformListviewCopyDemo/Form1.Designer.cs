31f01a88171b39d8c410df6fb5189bde2153d1d6
Tony Tang <tony.tang@semilab.com>
2021-10-04 12:18:02 -0400

    roughed in test code

--- WinformListviewCopyDemo/Form1.Designer.cs
+++ WinformListviewCopyDemo/Form1.Designer.cs
@@ -30,12 +30,90 @@ namespace WinformListviewCopyDemo
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.cityName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.countryName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lat = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lon = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 26);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cityName,
+            this.countryName,
+            this.lat,
+            this.lon});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(30, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(318, 186);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // cityName
+            // 
+            this.cityName.Text = "City";
+            this.cityName.Width = 61;
+            // 
+            // countryName
+            // 
+            this.countryName.Text = "Country";
+            // 
+            // lat
+            // 
+            this.lat.Text = "Latitude";
+            this.lat.Width = 70;
+            // 
+            // lon
+            // 
+            this.lon.Text = "Longitude";
+            this.lon.Width = 82;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(505, 342);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader cityName;
+        private System.Windows.Forms.ColumnHeader countryName;
+        private System.Windows.Forms.ColumnHeader lat;
+        private System.Windows.Forms.ColumnHeader lon;
     }
 }
 
--- WinformListviewCopyDemo/Form1.cs
+++ WinformListviewCopyDemo/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.ListViewItem;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WinformListviewCopyDemo
 {
@@ -15,6 +17,58 @@ namespace WinformListviewCopyDemo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection selectedItems = listView1.SelectedItems;
+
+            String text = "";
+            foreach (ListViewItem item in selectedItems)
+            {
+                foreach(ListViewSubItem subItem in item.SubItems)
+                {
+                    text += subItem.Text + ", ";
+                }
+                text += Environment.NewLine;
+            }
+
+            Clipboard.SetText(text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[,] array2d = new string[,]
+            {
+                {"Boston", "USA", "42.358056", "-71.063611" },
+                {"New York", "USA", "40.712778", "-74.006111" },
+                {"Paris", "France", "48.856613", "2.352222" },
+                {"London", "UK", "51.507222", "-0.1275" },
+                {"Syracuse", "USA", "43.046944", "-76.144444" }
+            };
+
+            for (int i = 0; i <= array2d.GetUpperBound(0); i++)
+            {
+                listView1.Items.Add(new ListViewItem((new CustomArray<string>()).GetRow(array2d, i)));
+            }
+
+        }
+    }
+
+    public class CustomArray<T>
+    {
+        public T[] GetColumn(T[,] matrix, int columnNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+                    .Select(x => matrix[x, columnNumber])
+                    .ToArray();
+        }
+
+        public T[] GetRow(T[,] matrix, int rowNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+                    .Select(x => matrix[rowNumber, x])
+                    .ToArray();
         }
     }
 }
